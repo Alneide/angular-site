9f1c7a6c4bc0a2311a89b56691c4b111e398e926
Alneide <109877622+Alneide@users.noreply.github.com>
2022-10-18 20:47:00 -0300

    testando componente card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
+import {CardModule} from 'primeng/card';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -13,7 +15,8 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
