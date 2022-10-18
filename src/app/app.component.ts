1ac97e1400ca29c67cb12432f2c2b97ccd501566
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-10-18 14:29:59 -0300

    primeiras modificacoes no app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-site';
+  title = 'Projeto em Angular Alneide & Jéssica';
 }
